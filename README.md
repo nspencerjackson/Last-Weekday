85dfc5544b4509c713b5481b13d3a9ceb2a8b965
nspencerjackson <nspencerjackson@gmail.com>
2021-12-06 13:35:52 +0800

    updated code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Last-Weekday
 Simple code to determine the date of the last weekday
+----------
+Need to make it so will look at previous year if needed
+----------
--- checkDay.py
+++ checkDay.py
@@ -1,1 +1,7 @@
-import  
+from datetime import date
+
+def lastDay(inMonth):
+    months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    for i in range(months[inMonth - 1]):
+        temp = date(2021, inMonth, (i + 1)).weekday()
+
