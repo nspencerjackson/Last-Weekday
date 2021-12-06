657de3d3279d91cc37aebc0defe1dc9bf13c391d
nspencerjackson <nspencerjackson@gmail.com>
2021-12-06 16:25:45 +0800

    Completed Code

--- checkDay.py
+++ checkDay.py
@@ -4,4 +4,17 @@ def lastDay(inMonth):
     months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     for i in range(months[inMonth - 1]):
         temp = date(2021, inMonth, (i + 1)).weekday()
+        if temp < 6:
+            friday = i
+    return friday
 
+def prevDay(inMonth, inDay):
+    months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    for i in range(months[inMonth - 1]):
+        temp = date(2021, inMonth, (i + 1)).weekday()
+        if temp < 6:
+            if (i+1) < inDay:
+                day = i
+            else:
+                break
+    return day
--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 from datetime import date
+from checkDay import *
 
-def lastDay
+print("What is the day?:")
+day = input()
+print("What is the month?:")
+m = input()
+pring("What is the year?:")
+y = input()
+
+x = date(int(y), int(m), int(day))
+print(x.weekday())
+
+if x.day == 1:
+    if x.weekday() == 0:
+        lastMonth = x.month - 1
+        prev = lastDay(lastMonth)
+elif x.day > 1:
+    if x.weekday() == 0:
+        lastMonth = x.month
+        prev = prevDay(x.month, x.day)
+    elif x.weekday() <= 5:
+        prev = x.day - 1
+        lastMonth = x.month
+
+s = date(y, lastMonth, prev)
+print("The last date of the week is:", s)
