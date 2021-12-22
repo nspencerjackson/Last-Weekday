8e5124e88d8c45b085cb516c11114db1fbb53fb0
nspencerjackson <nspencerjackson@gmail.com>
2021-12-22 12:31:56 +0800

    Fixed bug for 1st of Jan

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from datetime import date
 from checkDay import *
 
+# Gets the date
 print("What is the day?:")
 day = input()
 print("What is the month?:")
 m = input()
-pring("What is the year?:")
+print("What is the year?:")
 y = input()
 
+# makes x today's date
 x = date(int(y), int(m), int(day))
-print(x.weekday())
+print("the weekday is: ", x.weekday())
+print(x.day)
 
+# Checks if today is the first of the month
 if x.day == 1:
-    if x.weekday() == 0:
+    # Checks if it is the first day of a new year (make this a function)
+    if x.month == 1:
+        lastMonth = 12
+        prev = lastDay(lastMonth)
+        y = str(int(y) - 1)
+    else:
         lastMonth = x.month - 1
         prev = lastDay(lastMonth)
 elif x.day > 1:
@@ -20,8 +29,14 @@ elif x.day > 1:
         lastMonth = x.month
         prev = prevDay(x.month, x.day)
     elif x.weekday() <= 5:
+        print("now")
         prev = x.day - 1
         lastMonth = x.month
+        print("pot - ", lastMonth)
+    elif x.weekday() == 6:
+        prev = x.day - 2
+        lastMonth = x.month
+        print("ato - ", lastMonth)
 
-s = date(y, lastMonth, prev)
+s = date(int(y), int(lastMonth), int(prev))
 print("The last date of the week is:", s)
