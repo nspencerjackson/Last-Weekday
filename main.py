63c7005682d9ddf19e033c2ffcbadff7476cedab
nspencerjackson <nspencerjackson@gmail.com>
2021-12-22 19:45:22 +0800

    code working with new checkYear function

--- main.py
+++ main.py
@@ -11,32 +11,34 @@ y = input()
 
 # makes x today's date
 x = date(int(y), int(m), int(day))
-print("the weekday is: ", x.weekday())
-print(x.day)
 
 # Checks if today is the first of the month
 if x.day == 1:
     # Checks if it is the first day of a new year (make this a function)
     if x.month == 1:
-        lastMonth = 12
-        prev = lastDay(lastMonth)
-        y = str(int(y) - 1)
+        ls = checkYear(int(m), int(day), y)
     else:
         lastMonth = x.month - 1
-        prev = lastDay(lastMonth)
+        prev = lastDay(lastMonth, y)
 elif x.day > 1:
-    if x.weekday() == 0:
-        lastMonth = x.month
-        prev = prevDay(x.month, x.day)
-    elif x.weekday() <= 5:
-        print("now")
-        prev = x.day - 1
-        lastMonth = x.month
-        print("pot - ", lastMonth)
-    elif x.weekday() == 6:
-        prev = x.day - 2
-        lastMonth = x.month
-        print("ato - ", lastMonth)
+    if x.month == 1:
+        lastYearDate = checkYear(int(m), int(day), y)
+        lastMonth = lastYearDate.month
+        y = lastYearDate.year
+        prev = lastYearDate.day
+    else:
+        if x.weekday() == 0:
+            lastMonth = x.month
+            prev = prevDay(x.month, x.day, x.year)
+        elif x.weekday() <= 5:
+            print("now")
+            prev = x.day - 1
+            lastMonth = x.month
+            print("pot - ", lastMonth)
+        elif x.weekday() == 6:
+            prev = x.day - 2
+            lastMonth = x.month
+            print("ato - ", lastMonth)
 
 s = date(int(y), int(lastMonth), int(prev))
 print("The last date of the week is:", s)
